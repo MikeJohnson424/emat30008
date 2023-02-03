b763f16719caf068024d451ad748a6023d1b32e9
MikeJohnson424 <michaeljj2601@gmail.com>
2023-02-03 12:59:12 +0000

    Adjusted solve_to function to accept input argument that determines which method is used.

--- week14.py
+++ week14.py
@@ -13,7 +13,7 @@ def euler_step(h,x_n, f): # define function to compute a single euler step
     x_n1 = x_n + h*f
     return x_n1
 
-def RK_step(h,x,t,f):
+def RK_step(h,x,t,f): # h: time_step, x = current solution, t = current time, f = ODE function
     k1 = f(x,t)
     k2 = f(x+h*k1/2, t+h/2)
     k3 = f(x + h*k2/2, t+ h/2)
@@ -21,17 +21,15 @@ def RK_step(h,x,t,f):
     x_n1 = x + (h/6)*(k1+2*k2+2*k3+k4)
     return x_n1
 
+def solve_to(func, x0, t, deltat_max, method): # Method == 1: EULER, method == 2: RUNGE-KUTTA
 
-
-def solve_to(func, x0, t, deltat_max):
-
-    method = input('Please choose a numerical method: Runge-Kutta [1] Forward Euler [2]')
+    
     t_steps = int(t/deltat_max)
     x_old = x0
     x = np.array([])
 
-    if method == 2:  
-        # Perform integration of ODE function using Euler method in range t
+    if method == 1:  
+        # Perform integration of ODE function using EULER METHOD in range t
 
         for i in range(t_steps):
             x_new = euler_step(deltat_max,x_old,f(x_old, i*deltat_max))
@@ -39,7 +37,7 @@ def solve_to(func, x0, t, deltat_max):
             x = np.append(x, x_new)
         return [x,t_steps]
 
-    else:
+    else: # Perform integration of ODE function using RUNGE-KUTTA method in range t
         
         for i in range(t_steps):
             x_new = RK_step(deltat_max, x_old, i*deltat_max,f)
@@ -48,10 +46,12 @@ def solve_to(func, x0, t, deltat_max):
         return [x,t_steps]
 
 
-[x,t_steps] = solve_to(f,1,1,0.01)
+[x,t_steps] = solve_to(f,1,1,0.01,1)
 
 #%%
 
 plt.plot(range(t_steps),x)
 plt.show()
+
+
 
