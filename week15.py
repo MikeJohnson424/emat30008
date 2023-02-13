b825a6018a10af87379163818af921d0c1a22b8f
MikeJohnson424 <michaeljj2601@gmail.com>
2023-02-13 18:06:57 +0000

    Updated shooting code. Init is now 3x1 vector where elements 1 and 2 are intitial guesses for x and y, third element is initial guess for period of oscillation of limit cycle.

--- week15.py
+++ week15.py
@@ -16,8 +16,9 @@ def PPM(x,t=0,a=1,b=0.2,d=0.1):
 
 # At b = 0.26 a stable limit cycle is created
 
-[x,t_space] = solve_to(PPM,[1,2,1],100,0.01,2)
+[x,t_space] = solve_to(PPM,[0.4,0.4],100,0.01,2)
 
+#plt.plot(t_space,x[0])
 plt.plot(x[0],x[1])
 plt.show()
 
@@ -29,21 +30,29 @@ def limit_cycle_solver(init, func):
 
     def lim_cycle_problem(init):
         x0 = init[0:2]
-        [x, t_space] = solve_to(func,x0,100)
-        x_dot0 = x[1,0]
+        T = init[2]
+        [x, t_space] = solve_to(func,x0,T)
+        x_dot0 = PPM(x0)
+        dx_dt0 = x_dot0[0]
         sol = x[:,-1]
 
-        return np.hstack((x[:,0]-sol,x_dot0))
+        return np.hstack((x[:,0]-sol,dx_dt0))
 
-    ans = fsolve(lim_cycle_problem,[init])
+    ans = fsolve(lim_cycle_problem,init)
 
     return ans
 
-ans = limit_cycle_solver([1,2,1],PPM)
+
+#%%
+
+ans = limit_cycle_solver([1,0.5,2],PPM)
 
 print(ans)
 
+[x,t_space] = solve_to(PPM,ans[0:2],100,0.01,2)
 
+plt.plot(x[0],x[1])
+plt.show()
 
 
 # %%
