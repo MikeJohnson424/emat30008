2c435e39adda6e408b1e8d7ac87bcb304ae4d897
MikeJohnson424 <michaeljj2601@gmail.com>
2023-02-21 15:52:18 +0000

    set output of isole_lim_cycle as a class with attributes x0 and period. Set up possibility for alternate phase condition (x0 - 0.4 = 0)

--- limit_cyle.py
+++ limit_cyle.py
@@ -1,3 +1,5 @@
+#%%
+
 from scipy.optimize import fsolve
 from integrate import solve_to
 import numpy as np
@@ -9,6 +11,7 @@ def lim_cycle_problem(func,init):
     T = init[-1]
     sol = solve_to(func,x0,T).x[:,-1]
     phase_con = func(x0)[0]
+    phase_con1 = x0[0] - 0.4
 
     return np.hstack((x0-sol,phase_con))
 
@@ -17,4 +20,10 @@ def isolate_lim_cycle(func, init):
 
     ans = fsolve(lambda x: lim_cycle_problem(func, x),init)
 
+    class ans:
+        def __init__(self, period, x0) -> None:
+            self.period = ans[-1]
+            self.x0 = ans [0:-1]
+
     return ans
+
