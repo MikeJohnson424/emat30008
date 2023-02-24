2af30a9b7db9ff25b2320ea268b1ea7407716797
MikeJohnson424 <michaeljj2601@gmail.com>
2023-02-24 17:23:06 +0000

    working on developing unit tests using python's unittest module. Solve_to readjusted to accept strings as inputs to determine stepping method.

--- week16.py
+++ week16.py
@@ -3,23 +3,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from integrate import solve_to
-from limit_cycle import isolate_lim_cycle
 import unittest
-from ODEs import f,g,hopf_normal_form,PPM, VDP
-from scipy.integrate import solve_ivp
-from integrate import euler_step, RK_step
+from functions import f,g,hopf_normal_form,PPM, VDP, hopf_normal_form_sol
 
+result_true = hopf_normal_form_sol
+
+# %%
+
+t_true = np.linspace(0,10,100)
+
+x_true = hopf_normal_form_sol(t_true,1)
+
+
+result = solve_to(hopf_normal_form,[1,0],10)
+x = result.x
+t = result.t_space
 
 #%%
 
-result_euler = solve_to(hopf_normal_form, [10,10], 10, 0.0001,method = 'forward_euler')
-x_euler  = result_euler.x
 
-result_RK4 = solve_to(hopf_normal_form, [10,10], 10, 0.0001,method = 'ASD')
-x_RK4  = result_RK4.x
-
-
-plt.plot(x_euler[0],x_euler[1])
-plt.plot(x_RK4[0],x_RK4[1])
-
+plt.plot(t_true,x_true[0])
+plt.plot(t,x[0])
 # %%
