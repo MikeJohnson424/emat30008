1fc3861682074bee337da148a74f84fe41af7c62
MikeJohnson424 <michaeljj2601@gmail.com>
2023-04-14 20:05:27 +0100

    Updating code to store solution at every time interval. Animation of solution converging added. Code to implement a stable dt for any dx, D also implemented.

--- week20.py
+++ week20.py
@@ -4,32 +4,51 @@ import numpy as  np
 from week19 import construct_A_and_b, BoundaryCondition, Grid
 from math import ceil
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation 
 
-def explicit_diffusion_solver(grid,bc_left,bc_right,D,dt,t_steps):
+def explicit_diffusion_solver(grid,bc_left,bc_right,D,t_steps):
 
     dx = grid.dx
     [A,b] = construct_A_and_b(grid,bc_left,bc_right)
-    u = np.zeros(len(grid.x)-2)
+    u = np.zeros((len(grid.x)-2,t_steps+1))
+
+    u_old = u[:,0]
 
     for n in range(t_steps):
 
-        t = dt*t_steps # Current time-stamp
 
-        u_new = u + dt*D/dx**2*(np.dot(A,u)+b)
-        u = u_new
+        dt = 0.5*dx**2/D # Recalculate dt to ensure stability
+        t = dt*t_steps # Current time
+
+        u_new = u_old + dt*D/dx**2*(np.matmul(A,u_old)+b)
+        u_old = u_new
+
+        u[:,n+1] = u_new
         
     return u
 
 
 #%%
 
-bc_left = BoundaryCondition('dirichlet', 2)
-bc_right = BoundaryCondition('dirichlet', 5)
-grid = Grid(N=10, a=0, b=10)
+bc_left = BoundaryCondition('dirichlet', [0])
+bc_right = BoundaryCondition('dirichlet', [6])
+grid = Grid(N=100, a=0, b=10)
+x = grid.x
 
-u = explicit_diffusion_solver(grid,bc_left,bc_right,D=1,dt=0.1,t_steps=1000)
+u = explicit_diffusion_solver(grid,bc_left,bc_right,D=1,t_steps=1000)
 
-plt.plot(grid.x[1:-1],u, 'o', markersize = 2)
+plt.plot(grid.x[1:-1],u[:,-1], 'o', markersize = 2)
 
+# %%
+
+fig,ax = plt.subplots()
+
+line, = ax.plot(x[1:-1],u[:,0])
+
+def animate(i):
+    line.set_data((x[1:-1],u[:,i]))
+    return line,
+
+ani = FuncAnimation(fig, animate, frames=1000, interval=20, blit=True)
+plt.show()
 # %%
