544555339a44b7e0d9789f2523c59cb0d15b995c
MikeJohnson424 <michaeljj2601@gmail.com>
2023-04-21 12:06:44 +0100

    Discretisation argument obsolete so has been replaced with 'solve_for' where users can choose either equilibria or limit cycles.

--- continuation.py
+++ continuation.py
@@ -17,11 +17,11 @@ def predictor(u_current,u_old):
     u_pred = u_current + delta_u
     return [u_pred,delta_u]
 
-def corrector(myode,u,u_pred,delta_u,vary_par,par0,discretisation=None):
+def corrector(myode,u,u_pred,delta_u,vary_par,par0,solve_for=None):
 
     par0[vary_par] = u[-1]
 
-    if discretisation == shooting:
+    if solve_for == 'limit_cycle':
         R1 = shooting(myode,u[:-1],par0)
         R2 = np.dot(u - u_pred, delta_u)
     else:
@@ -29,9 +29,9 @@ def corrector(myode,u,u_pred,delta_u,vary_par,par0,discretisation=None):
         R2 = np.dot(u - u_pred, delta_u)
     return np.hstack((R1,R2))
 
-def find_initial_solutions(solver,myode,x0,par0,vary_par,step_size,discretisation):
+def find_initial_solutions(solver,myode,x0,par0,vary_par,step_size,solve_for):
 
-    if discretisation == shooting: # Solve for limit cycle conditions
+    if solve_for == 'limit_cycle': # Solve for limit cycle conditions
         u_old = np.hstack((
             solver(lambda x: shooting(myode,x,par0),x0).x,
             par0[vary_par]
@@ -61,13 +61,13 @@ def continuation(myode,  # the ODE to use
     vary_par=0,  # the index of the parameter to vary
     step_size=0.1,  # the size of the steps to take
     max_steps=50,  # the number of steps to take
-    discretisation=None,  # the discretisation to use
+    solve_for = 'equilibria', # 'equilibria' or 'limit cycle'
     solver=root):  # the solver to use
     
     x_dim = len(x0) # Dimension of the solution
     u = gen_sol_mat(x_dim,max_steps) # Initialise matrix to contain solution and varying parameter
 
-    u_old,u_current = find_initial_solutions(solver,myode,x0,par0,vary_par,step_size,discretisation)
+    u_old,u_current = find_initial_solutions(solver,myode,x0,par0,vary_par,step_size,solve_for)
 
     u[:,0] = u_old # Store first solution
 
@@ -79,7 +79,7 @@ def continuation(myode,  # the ODE to use
 
         u_pred,delta_u = predictor(u_current,u_old)
         u_true = solver(lambda u: 
-                        corrector(myode,u,u_pred,delta_u,vary_par,par0,discretisation),
+                        corrector(myode,u,u_pred,delta_u,vary_par,par0,solve_for),
                           u_pred).x
         
         # Update values for next iteration
@@ -121,13 +121,13 @@ myode = PPM; x0 = [5,6]; par0 = [0.5,0.2,0.1]; vary_par = 0; step_size = 0.1; ma
 
 # %%
 
-""" TEST FOR SHOOTING """
+""" TEST FOR LIMIT CYCLES """
 
 u = continuation(PPM,x0 = [0.5,0.5,20],par0 = [1,0.1,0.1],
                    vary_par = 0,
                    step_size = 0.1,
                    max_steps = 20,
-                   discretisation = shooting)
+                   solve_for = 'limit_cycle')
 plt.plot(u[-1],u[0])
 
 
