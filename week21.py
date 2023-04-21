471ab10337177d947b21b903c04e31e7aed48095
MikeJohnson424 <michaeljj2601@gmail.com>
2023-04-21 11:38:54 +0100

    Implicit solver working for euler, crank-nicolson and IMEX

--- week21.py
+++ week21.py
@@ -5,58 +5,86 @@ from week19 import construct_A_and_b, BoundaryCondition, Grid
 from math import ceil
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from week20 import InitialCondition
 
-
-def implicit_diffusion_solver(grid,bc_left,bc_right,D,dt,t_steps,method = 'euler'):
-
-    dx = grid.dx
-    x = grid.x
-    C = dt*D/dx**2 
-    dx = grid.dx # Grid spacing
-    [A,b] = construct_A_and_b(grid,bc_left,bc_right) # Construct A and b matrices
-    u = np.sin(np.pi*x[1:-1]) # Intial condition
+def q(x,t,u):
     
+    array_size = len(x)
+    t = np.full((array_size,1),t).flatten()
 
+    return np.ones(array_size)
+
+def implicit_diffusion_solver(grid,bc_left,bc_right,IC,D,dt,t_steps,method = 'euler'):
+
+    dx = grid.dx # Grid spacing
+    x = grid.x # Array of grid points
+    C = dt*D/dx**2 
+    [A,b] = construct_A_and_b(grid,bc_left,bc_right) # Construct A and b matrices
+    u = np.zeros((len(grid.x),t_steps+1)) # Initialise array to store solutions
+
+    # Set initial condition
+
+    if IC.IC_type == 'function':
+         u[:,0] = IC.initial_condition(x) # Set initial condition
+    if IC.IC_type == 'constant':
+        u[:,0] = IC.initial_condition*np.ones(len(grid.x)) # Set initial condition
+
+    # Remove rows from solution matrix if dirichlet boundary conditions are used
+
+    if bc_left.type == 'dirichlet':
+        u = u[:-1] 
+        x = x[1:]
+    if bc_right.type == 'dirichlet':
+        u = u[1:]  
+        x = x[:-1] 
+
+    u_old = u[:,0] # Set initial condition as old solution 
+    
     if method == 'euler':
 
         for n in range(t_steps):
 
             t = n*dt # Current time
 
-            u_new = np.linalg.solve(np.eye(len(A))-C*A,u+np.dot(C,b(t))) # Solve for u(n+1) using implicit method
-            u = u_new # Update u vector
+            u_new = np.linalg.solve(np.eye(len(A))-C*A,
+                                    u_old+C*b(t)) # Solve for u_n+1 using implicit method
+            u_old = u_new # Update u vector
 
-    elif method == ' crank-nicolson':
+    elif method == 'crank-nicolson':
 
         for n in range(t_steps):
 
             t = n*dt # Current time
             u_new = np.linalg.solve(np.eye(len(A))-C/2*A,
-                                    np.matmul((np.eye(len(A))+C/2*A),u)+np.dot(C,b(t)))
-            u = u_new # Update u vector
-       
-    return u
+                                    np.matmul((np.eye(len(A))+C/2*A),u_old)+np.dot(C,b(t)))
+            u_old = u_new # Update u vector
+    
+    elif method == 'IMEX':
+
+        for n in range(t_steps):
+
+            t = n*dt # Current time
+            u_new = np.linalg.solve(np.eye(len(A))-C*A,
+                                    u_old+C*b(t)+q(x,t,u_old)) 
+            u_old = u_new # Update u vector
+
+    return u_old
 
 # %%
 
 bc_left = BoundaryCondition('dirichlet', [lambda x: 0])
 bc_right = BoundaryCondition('dirichlet', [lambda x: 0])
+IC = InitialCondition(lambda x: np.sin(np.pi*x))
 grid = Grid(N=100, a=0, b=1)
 
-u = implicit_diffusion_solver(grid,bc_left,bc_right,D=0.1,dt=20,t_steps=10,method='crank-nicolson')
+u = implicit_diffusion_solver(grid,
+                              bc_left,
+                              bc_right,
+                              IC,
+                              D=0.1,
+                              dt=20,
+                              t_steps=20,
+                              method='IMEX')
 plt.plot(grid.x[1:-1],u, 'o', markersize = 2)
-
+plt.ylim(0,1)
 # %%
-
-
-def crank_nicolson(grid,bc_left,bc_right,D,dt,t_steps):
-
-    dx = grid.dx
-    C = dt*D/dx**2 
-    u = np.zeros(len(grid.x)-2) # Intialise u vector to zeros
-    dx = grid.dx # Grid spacing
-
-    [A,b] = construct_A_and_b(grid,bc_left,bc_right) # Construct A and b matrices
-
-
-    return u
